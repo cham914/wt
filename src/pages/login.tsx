ca1dfedd0ea027fd974e4fa8a05294b473889afa
unknown <abdullahomotoke@gmail.com>
2024-03-21 10:51:45 -0400

    update 3

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -37,6 +37,7 @@ export default function Login() {
 
   return (
     <>
+    
       <div className="content">
         <div className="form">
           <form method="POST" onSubmit={handleSubmit}>
